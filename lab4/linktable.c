43b767cee0f7b3c1ade84022292036014fdddd4f
AidanCheung <31981212+AidanCheung@users.noreply.github.com>
2017-10-16 22:33:11 +0800

    cmd linklist-table

--- lab4/linktable.c
+++ lab4/linktable.c
@@ -1,38 +1,139 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"linktable.h"
-/*
-typedef struct DataNode
+
+
+/*create linklist*/
+
+tLinkTable * CreateLinkTable()
 {
-	tLinkTableNode *pNext;
-	char* cmd;
-	char* desc;
-	int (*handler)();
-}tDataNode;
-*/
-tDataNode* FindCmd(tLinkTable* head,char* cmd)
-{
-	tDataNode *pNode = (tDataNode *)GetLinkTableHead(head);
-	while(pNode != NULL)
+	tLinkTable *head = (tLinkTable*)malloc(sizeof(tLinkTable));
+	if(head == NULL)
 	{
-		//if判断的时候不能使用两个bool型的变量比较
-		if(strcmp(pNode->cmd,cmd) == 0)
-		{
-			return pNode;
-		}
-		pNode = (tDataNode *)GetNextLinkTableNode(head,(tLinkTableNode *)pNode);
+		return NULL;
 	}
-	return NULL;
+	head->pHead = NULL;
+	head->pTail = NULL;
+	head->SumOfNode = 0;
+	return head;
 }
 
-/*show all cmd in linklist*/
-int ShowAllCmd(tLinkTable * head)
+/*Delete a LinkTable*/
+
+int DeleteLinkTable (tLinkTable *pLinkTable)
 {
-	tDataNode *pNode = (tDataNode *)GetLinkTableHead(head);
-	while(pNode != NULL)
+	if(pLinkTable == NULL)
 	{
-		printf("%s - %s\n",pNode->cmd,pNode->desc);
-		pNode = (tDataNode *)GetNextLinkTableNode(head,(tLinkTableNode *)pNode);
+		return FAILURE;
 	}
-	return 0;
+	while(pLinkTable->pHead !=NULL)
+	{
+		tLinkTableNode *pNode = pLinkTable->pHead;
+		pLinkTable->pHead = pLinkTable->pHead->pNext;
+		free(pNode);
+		pLinkTable->SumOfNode -= 1;
+	}
+	pLinkTable->pHead = NULL;
+	pLinkTable->pTail = NULL;
+	free(pLinkTable);
+	return SUCCESS;
 }
+
+
+/*Add a LinkTableNode to LinkTable*/
+
+int AddLinkTableNode(tLinkTable *pLinkTable,tLinkTableNode * pNode)
+{
+	if(pLinkTable == NULL || pNode == NULL)
+	{
+		return FAILURE;
+	}
+	pNode->pNext = NULL;
+	if(pLinkTable->pHead == NULL)
+	{
+		pLinkTable->pHead = pNode;
+	}
+	if(pLinkTable->pTail == NULL)
+	{
+		pLinkTable->pTail = pNode;
+	}
+	else
+	{
+		pLinkTable->pTail->pNext = pNode;
+		pLinkTable->pTail = pNode;
+	}
+	return SUCCESS;
+}
+
+
+/*Delete a LinkTableNode from LinkTabnle*/
+
+int DelLinkTableNode(tLinkTable *pLinkTable,tLinkTableNode * pNode)
+{
+	if(pLinkTable == NULL || pNode == NULL)
+	{
+		return FAILURE;
+	}
+	if(pLinkTable->pHead == pNode)
+	{
+		pLinkTable->pHead = pLinkTable->pHead->pNext;
+		pLinkTable->SumOfNode -= 1;
+		if(pLinkTable->SumOfNode == 0)
+		{
+			pLinkTable->pTail = NULL;
+		}
+		return SUCCESS;
+	}
+	tLinkTableNode *p =pLinkTable->pHead;
+	while(p != NULL)
+	{
+		if(p->pNext == pNode)
+		{
+			p->pNext = p->pNext->pNext;
+			pLinkTable->SumOfNode -= 1;
+			if(pLinkTable->SumOfNode == 0)
+			{
+				pLinkTable->pTail = NULL;
+			}
+			return SUCCESS;
+		}
+	}
+}
+
+
+/*get LinkTableHead*/
+
+tLinkTableNode * GetLinkTableHead(tLinkTable *pLinkTable)
+{
+	if(pLinkTable == NULL || pLinkTable->pHead == NULL)
+	{
+		return NULL;
+	}
+	tLinkTableNode *pNode = pLinkTable->pHead;
+	return pNode;
+}
+
+
+/*get next LinkTableNode*/
+
+tLinkTableNode * GetNextLinkTableNode(tLinkTable *pLinkTable,tLinkTableNode * pNode)
+{
+	if(pLinkTable == NULL || pLinkTable->pHead == NULL)
+	{
+		return NULL;
+	}
+	return pNode->pNext;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
